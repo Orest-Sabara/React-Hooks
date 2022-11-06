f43ab0eff174013d3f75dfe055ef0ac5b29af059
Orest-Sabara <2626orestsabara@gmail.com>
2022-11-06 13:18:40 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+      <div >
+         <h1>Counter</h1>
+          <button className='btn btn-success'>PLUS</button>
+          <button className='btn btn-danger'>MINUS</button>
+      </div>
+  )
+}
+
+export default App;
