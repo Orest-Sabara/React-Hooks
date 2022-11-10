9134239c13979ae2cb2d06b12cbf648202fdabcf
Orest-Sabara <2626orestsabara@gmail.com>
2022-11-10 19:35:30 +0200

    start App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 function App() {
-  return (
-      <div >
-         <h1>Counter</h1>
-          <button className='btn btn-success'>PLUS</button>
-          <button className='btn btn-danger'>MINUS</button>
-      </div>
-  )
+    return (
+
+        <blockquote className="blockquote text-center">
+            <p className="mb-20" style={{marginTop: '20px'}}>
+                You need to paste any file from example_hooks folder into App.js
+            </p>
+            <p className="blockquote-footer">
+                <cite title="Source Title">ctrl C --> ctrl V</cite> in App.js
+            </p>
+        </blockquote>
+
+    )
 }
 
 export default App;
